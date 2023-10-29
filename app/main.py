37aa4a85efe9e1a1c31ba464121de86092118db6
smbarr <szb5100@gmail.com>
2023-10-28 22:40:51 -0400

    Received request and send HTTP 200

--- app/main.py
+++ app/main.py
@@ -9,7 +9,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+
+    with conn:
+        data = conn.recv(144)
+        print(data)
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
